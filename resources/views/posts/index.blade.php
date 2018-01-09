8b67cd079914b1eaa13612c1fd0f3ecb6c32e6da
Phantom66 <leoturorellana@gmail.com>
2018-01-09 08:56:35 -0400

    Comprando usuario creador del post con el usuario autorizado en el index

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,24 +8,27 @@
           <h2>
             <a href="{{ route('post_path', ['key' => $key->id])}}"> {{ $key->title }}
             </a>
+
+              @if ($key->user_id ==Auth::user()->id)
+
+                <small class="pull-right">
+
+                  <a href="{{ route('edit_post_path', ['post'=>$key->id] ) }}" class="btn btn-info" >Update Post</a>
+
+                  {{-- Hay que emular el tipo de petición delete de la siguiente manera  --}}
+
+                  <form  action="{{ route('delete_post_path', ['post'=>$key->id] ) }}" method="POST">
+
+                    {{ csrf_field() }}
+                    {{ method_field('DELETE') }}
+
+                    <button type="submit" class="btn btn-danger">Delete</button>
+
+                  </form>
+
+                </small>
+              @endif
           </h2>
-          <small class="pull-right">
-
-            <a href="{{ route('edit_post_path', ['post'=>$key->id] ) }}" class="btn btn-info" >Update Post</a>
-
-            {{-- Hay que emular el tipo de petición delete de la siguiente manera  --}}
-
-            <form  action="{{ route('delete_post_path', ['post'=>$key->id] ) }}" method="POST">
-
-              {{ csrf_field() }}
-              {{ method_field('DELETE') }}
-
-              <button type="submit" class="btn btn-danger">Delete</button>
-
-            </form>
-
-            </small>
-
               <p>Posted  {{ $key->created_at->diffForHumans () }}</p>
 
       @endforeach
